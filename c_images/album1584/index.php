4cc3ac451de1ad47771ef84663eaeccd237fad23
Santiago Manuel <80847631+zSanti@users.noreply.github.com>
2022-07-12 03:01:57 +0200

    .
    .

---
+++ c_images/album1584/index.php
@@ -0,0 +1,14 @@
+<title>
+	     Acceso Denegado
+</title>
+
+
+
+<h1>Acceso Denegado</h1>
+<hr>
+
+<p>
+<blockquote>
+No tienes Permisos para ver este Archivo <!--#echo var="REDIRECT_URL"--> en este Servicio.
+</p>
+</blockquote>
